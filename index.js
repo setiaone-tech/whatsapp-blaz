882db3129f53a6049f3d4605d9416728a51e4fb3
bang L <71912420+setiaone-tech@users.noreply.github.com>
2021-02-11 19:16:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const csv = require('csv-parser');
-const results = [];
 const fs = require('fs');
 const random = require('random')
 const createCsvWriter = require('csv-writer').createArrayCsvWriter;
@@ -10,14 +9,6 @@ const csvWriter = createCsvWriter({
     header: ['NO', 'KONTAK'],
     path: 'kontak.csv'
 });
-
-//membaca kontak dari file csv dan memasukannya ke dalam array
-fs.createReadStream('kontak.csv')
-  .pipe(csv({separator : ','}))
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-	  console.log('Sukses Load');
-  });
 
 //pengecekan session whatsapp web
 const SESSION_FILE_PATH = './session.json';
@@ -57,19 +48,32 @@ client.on('message', async msg => {
     console.log('MESSAGE RECEIVED', msg);
 	//kirim pesan ke semua nomer yang berada di file kontak.csv
 	if (msg.body.startsWith('!sendto ')) {
-			// Direct send a new message to specific id
-			let bagi = msg.body.split(' ')[1];
-			let messageIndex = msg.body.indexOf(bagi);
-			let message = msg.body.slice(messageIndex, msg.body.length);
-			for (let i = 0;i < results.length; i++){
-				number = results[i]['KONTAK'];
-				number = number.includes('@c.us') ? number : `${number}@c.us`;
-				let chat = await msg.getChat();
-				chat.sendSeen();
-				client.sendMessage(number, message);
+			const results = [];
+			function r(results) {
+				//membaca kontak dari file csv dan memasukannya ke dalam array
+				fs.createReadStream('kontak.csv')
+				  .pipe(csv({separator : ','}))
+				  .on('data', (data) => results.push(data))
+				  .on('end', () => {
+					  console.log('Sukses Load');
+				  });
+				setTimeout (() => {
+					// Direct send a new message to specific id
+					let bagi = msg.body.split(' ')[1];
+					let messageIndex = msg.body.indexOf(bagi);
+					let message = msg.body.slice(messageIndex, msg.body.length);
+					for (let i = 0;i < results.length; i++){
+						number = results[i]['KONTAK'];
+						number = number.includes('@c.us') ? number : `${number}@c.us`;
+						client.sendMessage(number, message);
+					}
+				}, 1000);
+				setTimeout (() => {
+					client.sendMessage(msg.from, "Selesai mengirim ke "+results.length+" nomer!")
+				}, 3000);
 			}
+			r(results);
 	}
-	//mencari kontak nomer secara random
 	else if (msg.body.startsWith('!cek ')) {
 		let number = msg.body.split(' ')[1];
 		number = number.includes('@c.us') ? number : `${number}@c.us`;
@@ -81,26 +85,32 @@ client.on('message', async msg => {
 			}
 		})
 	}
+	//mencari kontak nomer secara random dengan perulangan
 	else if (msg.body.startsWith('!random ')) {
 		let wadah = [];
 		let provider = msg.body.split(' ')[1];
 		let req = msg.body.split(' ')[2];
-		for(let i=0;i < req;i++) {
-			let akhir = random.int(10000000,99999999);
-			let number = provider+akhir+'@c.us';
-			client.isRegisteredUser(number).then(function(isRegistered) {
-				if(isRegistered) {
-					client.sendMessage(msg.from, "*"+number+"*");
-				} else {
-					client.sendMessage(msg.from, "~"+number+"~");
-				}
-			})
-			let data = [i,provider+akhir];
-			wadah.push(data);
+		let berhasil = 0;
+		let gagal = 0;
+		function r(provider, req, wadah) {
+			for(let i=0;i < req;i++) {
+				let akhir = random.int(10000000,99999999);
+				let number = provider+akhir+'@c.us';
+				client.isRegisteredUser(number).then(function(isRegistered) {
+					if(isRegistered) {
+						berhasil += 1;
+						let data = [i, provider+akhir];
+						wadah.push(data);
+					} else {
+						gagal += 1;
+					}
+				});
+			}
+			setTimeout(() => {
+				csvWriter.writeRecords(wadah);
+				client.sendMessage(msg.from, "Dari "+req+" perulangan, ditemukan "+berhasil+" nomer dan "+gagal+" gagal.");
+			}, 3000);
 		}
-		csvWriter.writeRecords(wadah)
-			.then(() => {
-				client.sendMessage(msg.from, "Selesai!");
-		});
-		}
+		r(provider, req, wadah);
+	}
 });
